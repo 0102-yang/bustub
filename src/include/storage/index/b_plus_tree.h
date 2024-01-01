1a5084f1a30997f0e0967eef1623f6d5d5235bb0
0102-yang <1657375983@qq.com>
2024-01-01 13:53:18 +0800

    Finish Task2 of Buffer Pool(p1) of bustub fall/2023.

--- src/include/storage/index/b_plus_tree.h
+++ src/include/storage/index/b_plus_tree.h
@@ -129,7 +129,9 @@ class BPlusTree {
 
  private:
   /* Debug Routines for FREE!! */
-  void ToGraph(BPlusTreePage *page, BufferPoolManager *bpm, std::ofstream &out) const;
+  void ToGraph(page_id_t page_id, const BPlusTreePage *page, std::ofstream &out);
+
+  void PrintTree(page_id_t page_id, const BPlusTreePage *page);
 
   /**
    * @brief Convert A B+ tree into a Printable B+ tree
